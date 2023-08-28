f11e41feeadeda2306ef67e1f51d9b45a85ec6ab
Dev-Zod <131674780+Dev-Zod@users.noreply.github.com>
2023-08-28 10:44:16 +0200

    Update pricedashboard.py
    Starting point

--- pricedashboard.py
+++ pricedashboard.py
@@ -1,1 +1,40 @@
-pricedashboard
+package installations
+import pandas as pd
+import streamlit as st
+import yfinance as yf
+
+
+
+st.write("""
+# Stock Price Web Application
+
+ 
+Shown are the stock closing **price** and ***volume*** of Amazon!
+
+**Period**: May 2012 - May 2022
+         
+""")
+
+ticker_symbol = 'AMZN'
+
+#get ticker data by creating a ticker object
+
+ticker_data = yf.Ticker(ticker_symbol)
+
+#get amazon historical stock data for a specified time period as a dataframe
+
+tickerDF = ticker_data.history(period="1mo",
+                               interval="1d",start='2012-5-31', end= '2022-5-31')
+
+#columns: Open, High, Low Close, Volume, Dividends and Stock Splits
+
+st.write("""
+         ## Stock Closing Price in USD
+         """    )
+st.line_chart(tickerDF.Close)
+
+st.write("""
+         ## Stock Volume in USD
+         """    )
+st.line_chart(tickerDF.Volume)
+
